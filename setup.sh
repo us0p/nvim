af7336cdef9cf96048240b8347cf09ce0734ad78
us0p <lw98.lopes@gmail.com>
2025-12-18 14:14:08 -0300

    feature: adding setup script

---
+++ setup.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+set -e
+
+mkdir -p "$HOME/packages"
+cd "$HOME/packages"
+
+sudo pacman -Sy \
+  unzip \
+  ripgrep \
+  go
+
+export PROFILE="$HOME/.zshrc"
+
+# Install NVM
+curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+
+# Install Pyenv
+curl -fsSL https://pyenv.run | bash
+
+# Addin pyenv startup commands
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >>~/.zshrc
+echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >>~/.zshrc
+echo 'eval "$(pyenv init - zsh)"' >>~/.zshrc
+
+# Install Cargo
+curl https://sh.rustup.rs -sSf | sh
+
+# Refresh shell
+exec "$SHELL"
+
+# install latest node version and set's it as default
+nvm install --lts
+nvm alias default lts/*
+
+# install latest stable python
+pyenv install --latest
+pyenv global "$(pyenv latest)"
